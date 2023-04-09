4b8fc8dc5d6d6521847f352618ab39ae15e700a8
harshit kumar <harshitkk11@gmail.com>
2023-04-09 21:05:50 +0530

    spring-6-webapp

---
+++ src/test/java/harshit/springframework/Spring6WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package harshit.springframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
